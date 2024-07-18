3cfff7ef77d0b4b9df21d4ebf7170cdca58f12c0
adarsh <adarsh.chaudhary@gogroup.co>
2024-07-18 14:42:33 +0530

    Updates

--- assets/GoGroupLogo-C9FYDOFC.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-gsng2EmH.js";import{B as r}from"./HomePage-yTYOX_GF.js";import{T as s}from"./siteConstants-CzjBQurU.js";function n({textColor:i}){return o.jsxs(r,{display:"flex",children:[o.jsx(r,{className:"dot-circle"}),o.jsx(s,{color:i,marginInlineStart:1,lineHeight:1,variant:"h2",children:"GoGroup"})]})}export{n as G};
